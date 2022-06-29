0694159073af4c5573894db442ca432e0d194cb0
tpemba100 <tpemba.96@gmail.com>
2022-06-28 20:54:14 -0400

    third

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 
+import { Component } from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+//import {DISHES} from './shared/dishes';
 
 
-function App() {
-  return (
-    <div>
+class App extends Component () {
+
+  
+  
+  render () {
+
+    return (
+      <div>
       <Navbar dark color="primary">
         <div className ="container">
           <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
-    </div>
-  );
+      <Menu  dishes={this.state.dishes}/>
+      </div>
+      );
+  }  
 }
 
 export default App;
